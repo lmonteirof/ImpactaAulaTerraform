94398e2345ebda0689cca5feb2c7599b09cbb280
Leandro Monteiro <105895451+lmonteirof@users.noreply.github.com>
2022-07-12 20:58:09 -0300

    Delete main.tf

--- main.tf
+++
@@ -1,7 +0,0 @@
-module "turma08-app" {
-    source = "./modules"
-    cidr_block = "10.0.100.0/24"
-    cidr_block2 = "10.0.104.0/24"
-    project = "ProjetoLeandro"
-    vpc_name = "VPCDoLeandro"
-}
